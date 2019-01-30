aa74e91fd9199829def9f7cf249d9ea50f495778
Morgaux Meyer <mor@MultimediaPC.LabyrinthSolutions.local>
2019-01-31 08:45:55 +1300

    Installer v2

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 # Definitions
 SRCDIR="$(dirname $(readlink -f "$0"))"
 NAME="~/bin Installer"
-VERSION="1.0.0"
+VERSION="2.0.0"
 MESG="Installer commit"
 
 # Functions
@@ -24,16 +24,12 @@ main()
 
 usage()
 {
-	echo "usage:"
-	echo "./install.sh [ -h ] | [ -c | -p  MESSAGE ] | [ -i DIRECTORY ]"
+	echo "$NAME v$VERSION - usage:"
+	echo "./install.sh [ -h ] | [ -i DIRECTORY ]"
 	echo ""
 	echo "options:"
 	echo "-h, --help		show this message"
-	echo "-c message		commit local changes"
-	echo "-p message		push commits to repo"
-	echo "-i, --install DIRECTORY	install bin/DIRECTORY files"
-	echo ""
-	echo "$NAME - v$VERSION"
+	echo "-i, --install 		install files to ~/bin"
 	exit 0
 }
 
@@ -44,27 +40,9 @@ readFlags()
 			-h|--help)
 				usage # exit implicit
 				;;
-			-c|--commit)
-				shift
-				[ $# -gt 0 ] || die "-c requires message"
-				MESG="$@"
-				selfCommit
-				exit $?
-				;;
-			-p|--push)
-				shift
-				[ $# -gt 0 ] || die "-p requires message"
-				MESG="$@"
-				selfPush
-				exit $?
-				;;
 			-i|--install)
 				shift
-				[ $# -gt 0 ] || die "-i requires message"
-				while [ $# -gt 0 ] ; do
-					selfInstall $1
-					shift
-				done
+				selfInstall
 				;;
 			*)
 				die "$1 option unknown"
@@ -75,33 +53,10 @@ readFlags()
 
 selfInstall()
 {
-	if [ -d $1 ] ; then
-		for i in $1/* ; do
-			echo "Installing $i..."
-			cp -uf $SRCDIR/$i ~/bin/.
-		done
-		return
-	fi
-	die "directory $1 not found"
-}
-
-selfCommit()
-{
-	[ -x "$(command -v git)" ] || die "git not installed"
-	echo "Adding changes..."
-	git add .
-	echo "Commiting changes \"$MESG\""
-	git diff-index --quiet HEAD -- || git commit -m "$MESG"
-}
-
-selfPush()
-{
-	selfCommit
-	echo "Pushing commit..."
-	git log --oneline -1
-	#git push
-	until git push ; do
-		echo "Retrying" 1>&2
+	echo "Installing..."
+	for i in e tt uf weather ; do
+		echo "$i	->	~/bin"
+		cp -uf $SRCDIR/$i $HOME/bin/.
 	done
 }
 
