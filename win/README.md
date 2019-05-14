2d57717a5a2249cd46326e8857a43ff02c30ce9b
Morgaux Meyer <morgaux@raspi.local>
2019-05-15 11:20:19 +1200

    add win scripts, (good lord i hope i don't really use these)

---
+++ win/README.md
@@ -0,0 +1,4 @@
+# Windows batch files
+**Full disclaimer, I hate windows this is only for things good enough for me to
+ignore that fact.**
+
---
+++ win/WinVim.bat
@@ -0,0 +1,16 @@
+REM based on https://stackoverflow.com/questions/19497399/basic-text-editor-in-command-prompt
+REM as noted, he UI sucks but it has a soul
+
+@echo off
+title WinVim
+color a
+cls
+echo WinVim 1.02
+echo.
+echo To save press CTRL+Z then press enter
+echo.
+echo Make sure to include extension in file name
+set /p name=File Name:
+copy con %name%
+if exist %name% copy %name% + con
+
